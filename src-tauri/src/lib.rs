927f1f4215ba8204add8c2cf2d46d018d7887b58
immno <dancermno@gmail.com>
2024-12-27 10:55:47 +0800

    feat: 调整

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -2,19 +2,19 @@ pub mod errors;
 pub mod storage;
 pub mod models;
 
-// #[cfg_attr(mobile, tauri::mobile_entry_point)]
-// pub fn run() {
-//   tauri::Builder::default()
-//     .setup(|app| {
-//       if cfg!(debug_assertions) {
-//         app.handle().plugin(
-//           tauri_plugin_log::Builder::default()
-//             .level(log::LevelFilter::Info)
-//             .build(),
-//         )?;
-//       }
-//       Ok(())
-//     })
-//     .run(tauri::generate_context!())
-//     .expect("error while running tauri application");
-// }
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+  tauri::Builder::default()
+    .setup(|app| {
+      if cfg!(debug_assertions) {
+        app.handle().plugin(
+          tauri_plugin_log::Builder::default()
+            .level(log::LevelFilter::Info)
+            .build(),
+        )?;
+      }
+      Ok(())
+    })
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
+}
